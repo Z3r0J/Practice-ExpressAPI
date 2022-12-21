69e10d32cf06edfdf593662541a81f78cc9d0b11
Z3r0J <tibboxcentral@gmail.com>
2022-12-21 13:30:18 -0400

    AAA

--- src/endpoints/category/categoryEndpoints.js
+++ src/endpoints/category/categoryEndpoints.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { categoryDTO } from '../../services/category/categoryDto.js';
-import { createCategory, getAllCategory, getByIdCategory } from '../../services/category/categoryService.js';
+import { createCategory, getAllCategory, getByIdCategory, updateCategory } from '../../services/category/categoryService.js';
+import { validationResult, body } from 'express-validator';
+
 
 const categoryEndpoints = Router();
 
@@ -16,45 +17,75 @@ categoryEndpoints.get('/api/category/:id', async (req, res, next) => {
 
     if (id == 0) {
         res.statusCode = 400;
-        return res.json({ message: "El id debe ser un numero mayor que 0 y no debe contener letras" })
+        return res.status(400).json({ message: "El id debe ser un numero mayor que 0 y no debe contener letras" })
     }
 
     const categoryById = await getByIdCategory(id);
 
     if (categoryById) {
-        return res.json(categoryById);
+        return res.status(200).json(categoryById);
     }
 
-    res.statusCode = 400;
-    return res.json({ message: "No se encontro ninguna categoria con ese id" });
+    res.statusCode = 404;
+    return res.status(404).json({ message: "No se encontro ninguna categoria con ese id" });
 
-})
+});
 
-categoryEndpoints.post('/api/category', async (req, res, next) => {
-
-    console.log(req.body);
+categoryEndpoints.post('/api/category', [body("name" || "Name").trim().not().isEmpty().withMessage("Name is required"), body("description" || "Description").trim().not().isEmpty().withMessage("Description is required")], async (req, res, next) => {
+    if (!validationResult(req).isEmpty()) {
+        const error = new Error("Validation failed, entered data is incorrect");
+        error.statusCode = 422;
+        error.data = validationResult(req).array();
+        return res.status(422).json(error);
+    }
 
     const categoryName = req.body?.name || req.body?.Name;
     const categoryDescription = req.body?.description || req?.body.Description;
-
-    if (!categoryName) {
-        res.statusCode = 400;
-        return res.json({ message: "Name is required." });
-    }
-
-    if (!categoryDescription) {
-        res.statusCode = 400;
-        return res.json({ message: "Description is required" });
-    }
 
     const created = await createCategory({ categoryName, categoryDescription });
     if (created) {
         res.statusCode = 201;
 
-        return res.json({ message: "Creado correctamente" });
+        return res.status(201).json({ message: "Creado correctamente" });
     }
 
-})
+});
+
+categoryEndpoints.put('/api/category', [body("name" || "Name").trim().not().isEmpty().withMessage("Name is required"), body("description" || "Description").trim().not().isEmpty().withMessage("Description is required"), body("id" || "Id").trim().not().isEmpty().withMessage("Id is required")], async (req, res, next) => {
+
+    if (!validationResult(req).isEmpty()) {
+        const error = new Error("Validation failed, entered data is incorrect");
+        error.statusCode = 422;
+        error.data = validationResult(req).array();
+        return res.status(422).json(error);
+    }
+
+    const categoryName = req.body.name || req.body.Name;
+    const categoryDescription = req.body.description || req.body.Description;
+
+    const Id = Number(req.body.id || req.body.Id).toString() !== 'NaN' ? Number(req.body.id) : 0;
+
+    if (Id == 0) {
+        res.statusCode = 400;
+        return res.status(400).json({ message: "El id debe ser un numero mayor que 0 y no debe contener letras" })
+    }
+
+    console.log(Id)
+
+    const exist = await getByIdCategory(Id);
+
+    if (exist) {
+
+        const wasSuccess = await updateCategory({ categoryName, categoryDescription }, Id);
+        if (wasSuccess) {
+            return res.status(204).json();
+        }
+
+        return res.status(500).json({ message: "Something went wrongs..." })
+    }
+
+    return res.status(404).json({ message: "The category was not found" })
+});
 
 
 export default categoryEndpoints;
--- src/services/category/categoryService.js
+++ src/services/category/categoryService.js
@@ -2,7 +2,7 @@ import { categoryDTO } from "./categoryDto.js";
 
 export const getAllCategory = async () => {
     const category = [];
-    await categoryDTO.findAll({}).then(c => { return c.map(cat => category.push(cat.dataValues)) })
+    await categoryDTO.findAll({ include: ["products"] }).then(c => { return c.map(cat => category.push(cat.dataValues)) })
     console.log(category);
     return category;
 }
@@ -10,7 +10,7 @@ export const getAllCategory = async () => {
 export const getByIdCategory = async (id) => {
     let category = null;
 
-    await categoryDTO.findByPk(id).then((c) => category = c ? c.dataValues : null);
+    await categoryDTO.findByPk(id, { include: ["products"] }).then((c) => category = c ? c.dataValues : null);
 
     return category;
 }
@@ -22,6 +22,15 @@ export const createCategory = async (category) => {
         Name: category.categoryName,
         Description: category.categoryDescription
     }).then(() => { success = true }).catch(() => { success = false });
+
+    return success;
+}
+
+export const updateCategory = async (category, id) => {
+    let success = false;
+    await categoryDTO.update({ Name: category.categoryName, Description: category.categoryDescription }, { where: { Id: id } })
+        .then(() => { success = true })
+        .catch(() => { success = false });
 
     return success;
 }
